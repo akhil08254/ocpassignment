070ab6618f72057be7a0030de79cc4e08b7c89f0
akhil08254 <35929448+akhil08254@users.noreply.github.com>
2021-07-08 17:05:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,7 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-echo gethostname() . "\n";
-echo 'User IP Address - '. $_SERVER['REMOTE_ADDR'] . "\n";
-// server address
-$ip_server = $_SERVER['SERVER_ADDR'];
 // Printing the stored address
 $host= gethostname();
 $ip = gethostbyname($host);
-echo "Server IP Address is: $ip " . "\n";
+echo "POD IP Address is: $ip " . "\n";
 ?>
